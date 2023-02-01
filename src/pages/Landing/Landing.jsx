c5f9b4ee259b086aaa620cb467b9601da4718c52
User Name <kauth333@gmail.com>
2023-02-01 02:13:04 -0500

    add content in landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,9 +1,13 @@
+import "./Landing .css"
 const Landing = () => {
     return (
         <main>
         <h1>KAUTHAR MUMIN</h1>
+        <img src="assets/linkpic.jpg" alt="profile-pic" style={{ objectFit: 'cover', width: 350, height: 350} }/>
+        <div id="sub-head">
         <h2>Software Engineer</h2>
-        <h3>A solutions-driven engineer seeking new opportunities to learn and grow.</h3>
+        <h3><i>A solutions-driven engineer seeking new opportunities to learn and grow.</i></h3>
+        </div>
         </main>
         
     );
