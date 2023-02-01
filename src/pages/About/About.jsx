9e58ea145763b5982db3908e7d083d4b86583b3e
User Name <kauth333@gmail.com>
2023-02-01 03:28:54 -0500

    Added content to About Page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,6 +2,32 @@ const About = () => {
     return (
         <>
         <h1>About</h1>
+        <div className="personal">
+        <p>I'm a software engineer passionate about learning new technologies. My adaptability, ability to empathize and communicate well in collaborative environments makes me an excellent addition to any team. Because of my public health background, I’m highly motivated to creating solutions-driven applications that have a positive impact on users’ quality of life.</p>
+        </div>
+        
+        <div className="facts">
+        <h2>Interesting Facts About Myself</h2>
+        <ul>
+            <li>I used to play the cello all through high school.</li>
+            <li>Mother to a middle-aged orange Tabby. </li>
+            <li>Helped deliver a few newborns as a volunteer at the Edna Adan Hospital.</li>
+        </ul>
+        </div>
+       
+        <div className="shows">
+        <h2>Favorite Shows</h2>
+        <ul>
+            <li>Dark</li>
+            <li>The Office</li>
+            <li>Better Call Saul</li>
+            <li>The Great British Bake-Off</li>
+            <li>Fargo</li>
+        </ul>
+        </div>
+        <img src="assets/about-pic.jpeg" alt="about-pic" />
+
+
         </>
     );
 }
